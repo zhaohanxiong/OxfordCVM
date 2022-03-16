a20d1b696d38105616cd0df6ea2da2acbb6c15ce
Zhao Xiong <zhaohanx@hotmail.com>
2022-03-16 13:52:45 +0000

    initial commit

---
+++ temp.R
@@ -0,0 +1,6 @@
+x = 1
+y = 2
+
+print(x + y)
+
+plot(1:4, 1:4, type = "l")
---
+++ temp.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = 1
+y = 2
+
+
+print(x + y)
