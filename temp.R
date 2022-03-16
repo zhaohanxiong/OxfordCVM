c5d0ec06fb5f1b5b59ab8bc6eaa1ac9c60bfb169
Zhao Xiong <zhaohanx@hotmail.com>
2022-03-16 15:13:49 +0000

    mew

--- temp.R
+++
@@ -1,6 +0,0 @@
-x = 1
-y = 2
-
-print(x + y)
-
-plot(1:4, 1:4, type = "l")
--- temp.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-x = 1
-y = 2
-
-
-print(x + y)
