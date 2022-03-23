8007f42fb031b9cae32029b8953ba68c877b948a
Zhao Xiong <zhaohanx@hotmail.com>
2022-03-23 17:20:59 +0000

    planning implementation

--- main_PCA.R
+++ main_PCA.R
@@ -1,2 +1,19 @@
 
-# 
+#http://www.bioconductor.org/packages/devel/bioc/vignettes/scPCA/inst/doc/scpca_intro.html
+
+# functions  to load UKB datasets
+
+# # use function to produce UKB variables
+  
+# # use function to subset UKB dataset variables given outputs above
+  
+# # use function to convert into neuroPM toolbox inputs (3 files)
+
+# compute neighborhood variance
+# # 
+
+# functions to perform cPCA
+# # 
+
+# calculate pseudotime score
+# # 
