af9780584716f944adbbda5e90dc148f4607312b
Zhao Xiong <zhaohanx@hotmail.com>
2022-03-24 18:25:09 +0000

    refractored all preprocessing code, cleaned up directory

--- src/main_analysis.R
+++ src/main_analysis.R
@@ -1,6 +1,7 @@
 
 # load functions
 source("preprocess_filter_dataset.R")
+source("preprocess_neuroPM.R")
 
 # load UKB datasets
 ukb = load_raw_ukb_patient_dataset(path_ukb_data = "../../bb_data.csv",
@@ -16,9 +17,9 @@ ukb_filtered_rows = get_ukb_subset_rows(df = ukb$ukb_data,
                                         subset_option = "all")
 
 # subset UKB dataframe based on row/column filters, and remove missing
-ukb_df = return_cols_rows_filter_dataset(df = ukb$ukb_data,
-                                         cols = ukb_filtered_cols,
-                                         rows = ukb_filtered_rows)
+ukb_df = return_cols_rows_filter_df(df = ukb$ukb_data,
+                                    cols = ukb_filtered_cols,
+                                    rows = ukb_filtered_rows)
 #rm(ukb) # delete UKB variable from workspace to save RAM
 
 # clean dataset of rows/columns with too many missing values
@@ -27,10 +28,20 @@ ukb_df = return_clean_NA_from_df(df = ukb_df,
                                  threshold_row = 0.05)
 
 # get corresponding vector of labels depending on criteria
-bp_group = get_ukb_target_background_labels(df_subset = ukb_df,
-                                            target_criteria = "> 140/80")
+ukb_df = return_ukb_target_background_labels(df_subset = ukb_df,
+                                             target_criteria = "> 140/80")
 
-# # use function to convert into neuroPM toolbox inputs (3 files)
+##### for testing with NeuroPM Box
+# reduce computational cost by only taking a fraction of whole dataset
+# use function to convert and wrute into neuroPM toolbox inputs files (3 files)
+if (FALSE) {
+  ukb_df_small = return_fractional_df(ukb_df, 3000)
+  
+  neuroPM_write_all_df(ukb_df_small[,5:ncol(ukb_df_small)], # from 5th column
+                       labels = ukb_df_small$bp_group,
+                       path = "../../NeuroPM_cPCA_files")
+
+}
 
 # compute neighborhood variance
 # # 
