9eee637d654a18455fa989faa96b83364f9223d9
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-04 19:05:45 +0100

    executable of neuroPM setup but not really working

--- src/fmrib/run.sh
+++ src/fmrib/run.sh
@@ -4,7 +4,7 @@
 module add MATLAB/2020a
 
 # compile code and include sub-directories
-mcc -m run_NeuroPM.m -a ./cTI-codes/auxiliary/*.m -a cTI-codes/dijkstra_tools/*.m -a cTI-codes/*.m
+mcc -m run_NeuroPM.m -a ./cTI-codes/auxiliary/*.m -a cTI-codes/dijkstra_tools/*.m -a ./cTI-codes/*.m
 
 # execute the compiled program
 ./run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
