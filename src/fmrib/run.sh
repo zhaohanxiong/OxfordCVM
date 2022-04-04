2d11635eaeeb5ab032ce7d493bbe17dc6d33c971
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-05 00:26:03 +0100

    simplify addpath in run.sh again...

--- src/fmrib/run.sh
+++ src/fmrib/run.sh
@@ -4,7 +4,7 @@
 module add MATLAB/2020a
 
 # compile code and include sub-directories
-mcc -m run_NeuroPM.m -a ./io/ -a ./cTI-codes/
+mcc -m run_NeuroPM.m -a ./cTI-codes/
 
 # execute the compiled program
 ./run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
