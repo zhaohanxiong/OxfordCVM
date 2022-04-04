dc6491c27fe77f16dbaf693eec9896b7cb8c8a80
Zhao Xiong <zhaohanx@hotmail.com>
2022-04-04 18:27:59 +0100

    shell script to run several commands

---
+++ src/fmrib/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# add matlab module to workspace
+module add MATLAB/2020a
+
+# compile code and include sub-directories
+mcc -m run_NeuroPM.m -a ./cTI-codes/auxiliary/*.m -a cTI-codes/dijkstra_tools/*.m -a cTI-codes/*.m
+
+# execute the compiled program
+./run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
