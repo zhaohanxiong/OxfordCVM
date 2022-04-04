ed1ed5f6cbee8da4f5066a188f8faf679e6e69f1
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-04 13:34:06 +0100

    compiled test for running matlab

--- fmrib/temp.m
+++ fmrib/temp.m
@@ -1,9 +1,7 @@
-x = 2;
-y = 3;
-z = x + y;
 
-disp(z)
-
+x = 3;
+z = zscore([1,3,4,5,6,6])
+y = 2
 % module add MATLAB/2020a
 % mcc -m temp.m
 % ./run_temp.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
