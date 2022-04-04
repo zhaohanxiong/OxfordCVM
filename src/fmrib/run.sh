9e150a14890aeae08962605bf61bf603a11ffc2b
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-05 00:14:45 +0100

    successfully ran neuroPM source code in fmrib server

--- src/fmrib/run.sh
+++ src/fmrib/run.sh
@@ -9,6 +9,3 @@ mcc -m run_NeuroPM.m -a './io/' -a ./cTI-codes/*.m \
 
 # execute the compiled program
 ./run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
-
-https://www.google.com/search?q=matlab+mcc+addpath&ei=oXFLYpb6BpqChbIPm5mS2AY&ved=0ahUKEwjWzuL0uvv2AhUaQUEAHZuMBGsQ4dUDCA4&uact=5&oq=matlab+mcc+addpath&gs_lcp=Cgdnd3Mtd2l6EAMyBggAEBYQHjIICAAQCBANEB46BwgAEEcQsAM6BQgAEIAESgQIQRgASgQIRhgAUP4DWNwRYJoTaAFwAXgAgAFiiAHQBJIBATiYAQCgAQHIAQjAAQE&sclient=gws-wiz
-https://www.google.com/search?q=matlab+mcc+Unable+to+find+or+open+...+Check+the+path+and+filename+or+file+permissions.&ei=hXpLYv6QBcubgQb8m7-oDQ&ved=0ahUKEwi-54Syw_v2AhXLTcAKHfzND9UQ4dUDCA4&uact=5&oq=matlab+mcc+Unable+to+find+or+open+...+Check+the+path+and+filename+or+file+permissions.&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsANKBAhBGABKBAhGGABQN1jOCmD8C2gBcAF4AIABR4gBuwKSAQE1mAEAoAEByAEIwAEB&sclient=gws-wiz
