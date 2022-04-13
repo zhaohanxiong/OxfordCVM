3dc49be669673e6ae1e58634f907334abebfe3cc
Zhao Xiong <zhaohanx@hotmail.com>
2022-04-13 16:10:05 +0100

    new preprocessing file for entire ukb dataset

---
+++ src/fmrib/main_ukb_raw.R
@@ -0,0 +1,26 @@
+
+# load dataframe
+df = read.csv("../../../ukb51139.csv", sep = ",",
+              header = TRUE, nrows = 1000)
+
+# convert empties into missing value
+df[df==""] = NA
+
+# print information regarding outputs
+print(sprintf("Percentage of Missing Data Before Filtering %0.1f%%",
+              sum(is.na(df))/prod(dim(df))*100))
+print(sprintf("Number of Samples Before Filtering %0.0f", nrow(df)))
+
+# keep columns with under 50% missing data
+df = df[, colMeans(is.na(df)) <= 0.5]
+
+# keep rows with under 5% missing data
+df = df[rowMeans(is.na(df)) <= 0.05, ]
+
+# print information regarding outputs
+print(sprintf("Percentage of Missing Data After Filtering %0.1f%%",
+              sum(is.na(df))/prod(dim(df))*100))
+print(sprintf("Number of Samples After Filtering %0.0f", nrow(df)))
+
+# write to output
+write.csv(df,"../../../ukb51139_subset.csv", row.names = FALSE)
