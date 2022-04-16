c34ab962dfbbf3b7b539a90aee9afd3bc986d95e
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-16 19:00:29 +0100

    adjusting row and column missing value filters

--- src/fmrib/main_preprocess.R
+++ src/fmrib/main_preprocess.R
@@ -31,7 +31,7 @@ print(sprintf("Subset Data Frame is of Size %0.0f by %0.0f",
 
 # clean dataset of rows/columns with too many missing values
 ukb_df = return_clean_df(df = ukb_df,
-                         threshold_col = 0.5, threshold_row = 0.05,
+                         threshold_col = 0.75, threshold_row = 0.05,
                          char_cols = c(1))
 
 # remove rows with missing blood pressure values
@@ -40,7 +40,7 @@ ukb_df = ukb_df[(!is.na(ukb_df$`BPSys-2.0`)) & (!is.na(ukb_df$`BPDia-2.0`)),]
 # remove outliers
 ukb_df[, 2:ncol(ukb_df)] = return_remove_outlier(data =
                                                     ukb_df[, 2:ncol(ukb_df)])
-                                                    
+
 # display final dataframe size
 print(sprintf("Cleaned Data Frame is of Size %0.0f by %0.0f",
                                                     nrow(ukb_df), ncol(ukb_df)))
