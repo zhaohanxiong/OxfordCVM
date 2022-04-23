066a90f43abe5305b21e9bbfeb7376f4f6690ee6
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-23 19:32:32 +0100

    ukb_subsetted

--- src/fmrib/ukb_whole_data_subset.R
+++ src/fmrib/ukb_whole_data_subset.R
@@ -31,12 +31,12 @@ rm(ukb)
 # remove rows with missing blood pressure values
 ukb_df = ukb_df[(!is.na(ukb_df$`BPSys-2.0`)) & (!is.na(ukb_df$`BPDia-2.0`)),]
 
-# clean dataset of rows with too many missing values (less than 2.5% data)
-ukb_df = ukb_df[rowMeans(is.na(ukb_df)) <= 0.975, ]
+# clean dataset of rows with too many missing values (less than 5% data)
+ukb_df = ukb_df[rowMeans(is.na(ukb_df)) <= 0.95, ]
 
 # display subset dataframe size
 print(sprintf("Subset Data Frame is of Size %0.0f by %0.0f",
                                                     nrow(ukb_df), ncol(ukb_df)))
 
 # write to output (data & labels)
-fwrite(ukb_df, "../../../ukb51139_subset.csv")
+#fwrite(ukb_df, "../../../ukb51139_subset.csv")
