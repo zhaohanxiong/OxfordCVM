1b7b45f88cdec64ac22e8b58f4b85c044eee6904
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-04-23 12:16:59 +0100

    update run all shell script for nohup

--- src/fmrib/run_all.sh
+++ src/fmrib/run_all.sh
@@ -5,4 +5,4 @@ Rscript main_preprocess.R
 
 # execute the compiled matlab program
 module add MATLAB/2020a
-./NeuroPM/run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
+nohup ./NeuroPM/run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
