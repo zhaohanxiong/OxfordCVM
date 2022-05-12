b0659eaea7b1ad3460b37d6250936e2affa168ca
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-05-12 14:19:26 +0100

    added more automation to compile script to remove useless files

--- src/fmrib/NeuroPM/compile_NeuroPM.sh
+++ src/fmrib/NeuroPM/compile_NeuroPM.sh
@@ -5,3 +5,6 @@ module add MATLAB/2020a
 
 # compile code and include sub-directories
 mcc -m run_NeuroPM.m -a ./cTI-codes/
+
+# remove useless output files
+rm mccExcludedFiles.log readme.txt requiredMCRProducts.txt
