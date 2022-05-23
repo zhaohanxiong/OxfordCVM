43b6260b508810767071c83eeb0b278afa8808cd
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-05-23 19:10:49 +0100

    update run all shell file to include compiling if needed

--- src/fmrib/run_all.sh
+++ src/fmrib/run_all.sh
@@ -6,6 +6,11 @@
 # run R preprocessing script, writes to NeuroPM/io directory
 Rscript main_preprocess.R
 
-# execute the compiled matlab program
+# navigate to directory containing source code
 cd ./NeuroPM
+
+# compile matlab script (only if there were code changes)
+#./compile_NeuroPM.sh
+
+# execute the compiled matlab program
 nohup ./run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
