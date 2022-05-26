7d6a933ca3c3ff074f79f8224888e15e9e9e07b1
Zhao Xiong <zhaohanx@hotmail.com>
2022-05-26 20:17:28 +0100

    updated with 5 sub-runs, cPCA now working for 15k patients

--- src/fmrib/NeuroPM/merge_cPC.m
+++ src/fmrib/NeuroPM/merge_cPC.m
@@ -1,6 +1,6 @@
 addpath("cTI-codes\","cTI-codes\auxiliary\","cTI-codes\dijkstra_tools\");
 
-N_nodes = 12000;
+N_nodes = 15000;
 
 % load and combine the labels
 labels_0001 = readtable('C:/Users/zxiong/Desktop/io 1-3000/labels.csv');
@@ -23,16 +23,22 @@ bp_group = table2array(labels_9001(:, 'bp_group'));
 starting_point_9001 = find(bp_group == 1)+9000;
 final_subjects_9001 = find(bp_group == 2)+9000;
 
-starting_point = [starting_point_0001; starting_point_3001; starting_point_6001; starting_point_9001];
-final_subjects = [final_subjects_0001; final_subjects_3001; final_subjects_6001; final_subjects_9001];
-labels = [labels_0001; labels_3001; labels_6001; labels_9001];
+labels_12001 = readtable('C:/Users/zxiong/Desktop/io 12001-15000/labels.csv');
+bp_group = table2array(labels_12001(:, 'bp_group'));
+starting_point_12001 = find(bp_group == 1)+12000;
+final_subjects_12001 = find(bp_group == 2)+12000;
+
+starting_point = [starting_point_0001; starting_point_3001; starting_point_6001; starting_point_9001; starting_point_12001];
+final_subjects = [final_subjects_0001; final_subjects_3001; final_subjects_6001; final_subjects_9001; final_subjects_12001];
+labels = [labels_0001; labels_3001; labels_6001; labels_9001; labels_12001];
 
 % load and combine the cPCs
 mappedX_0001 = padarray(mappedX_0001, [0 4], 0, 'post');
 mappedX_3001 = padarray(mappedX_3001, [0 5], 0, 'post');
 mappedX_6001 = padarray(mappedX_6001, [0 5], 0, 'post');
 mappedX_9001 = padarray(mappedX_9001, [0 6], 0, 'post');
-mappedX = [mappedX_0001; mappedX_3001; mappedX_6001; mappedX_9001];
+mappedX_12001 = padarray(mappedX_12001, [0 5], 0, 'post');
+mappedX = [mappedX_0001; mappedX_3001; mappedX_6001; mappedX_9001; mappedX_12001];
 %mappedX(abs(mappedX) > std(mappedX,0,"all")*3) = 0;
 
 %%
@@ -60,31 +66,6 @@ MST = full(Tree + Tree');
 datas = dijkstra(MST, Root_node');
 max_distance = max(datas.A(~isinf(datas.A)));
 global_pseudotimes(in_background_target,1) = datas.A/max_distance;
-
-% compute between group
-% in_background_target_0001 = [starting_point_0001(:); final_subjects_0001(:)];
-% out_background_target_0001 = setdiff(1:3000, in_background_target_0001)';
-% temp_dist = dist_matrix0(out_background_target_0001,in_background_target_0001);
-% [i,j] = min(temp_dist,[],2);
-% global_pseudotimes(out_background_target_0001,1) = global_pseudotimes(in_background_target_0001(j),1);
-% 
-% in_background_target_3001 = [starting_point_3001(:); final_subjects_3001(:)];
-% out_background_target_3001 = setdiff(3001:6000, in_background_target_3001)';
-% temp_dist = dist_matrix0(out_background_target_3001,in_background_target_3001);
-% [i,j] = min(temp_dist,[],2);
-% global_pseudotimes(out_background_target_3001,1) = global_pseudotimes(in_background_target_3001(j),1);
-% 
-% in_background_target_6001 = [starting_point_6001(:); final_subjects_6001(:)];
-% out_background_target_6001 = setdiff(6001:9000, in_background_target_6001)';
-% temp_dist = dist_matrix0(out_background_target_6001,in_background_target_6001);
-% [i,j] = min(temp_dist,[],2);
-% global_pseudotimes(out_background_target_6001,1) = global_pseudotimes(in_background_target_6001(j),1);
-% 
-% in_background_target_9001 = [starting_point_9001(:); final_subjects_9001(:)];
-% out_background_target_9001 = setdiff(9001:12000, in_background_target_9001)';
-% temp_dist = dist_matrix0(out_background_target_9001,in_background_target_9001);
-% [i,j] = min(temp_dist,[],2);
-% global_pseudotimes(out_background_target_9001,1) = global_pseudotimes(in_background_target_9001(j),1);
 
 out_background_target = setdiff(1:N_nodes,in_background_target)';
 temp_dist = dist_matrix0(out_background_target,in_background_target);
