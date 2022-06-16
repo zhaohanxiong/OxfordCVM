d19f9186af005f0be6ee67021d5dbaaf5f723f51
Zhao Xiong <zhaohanx@hotmail.com>
2022-06-16 15:26:22 +0100

    updated plotlty plottting slightly

--- src/main_trajectory.py
+++ src/main_trajectory.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 from scipy.sparse.csgraph import laplacian
 
 # source path & set the current working directory
-path = "fmrib/NeuroPM/io/"
+path = "src/fmrib/NeuroPM/io/"
 os.chdir(path)
 
 # load labels (0 = between, 1 = background, 2 = disease)
@@ -24,7 +24,7 @@ MST_label = pd.read_csv("MST.csv",index_col=False)
 root_node = np.argmin(MST_label["pseudotime"])
 
 # load dijkstra for determining trajectories of each path
-dijkstra_F = scipy.io.loadmat("dijkstra.mat")["dijkstra_F"][:,0]
+#dijkstra_F = scipy.io.loadmat("dijkstra.mat")["dijkstra_F"][:,0]
 
 # determine the trajectory route of each node from the root node
 # - find nodes which are not father nodes, these are the most extreme points, use this to back track to the root node.
@@ -36,23 +36,26 @@ dijkstra_F = scipy.io.loadmat("dijkstra.mat")["dijkstra_F"][:,0]
 # infer the traj of between group with the same pseudotime score as the background/target nodes
 
 
-# compute spectral layout using lapacian and eigen decomp
+# compute spectral layout using lapacian and eigen decomp (1 minute run time)
 L = laplacian((MST_mat>0).astype(int))
 vals, vecs = np.linalg.eigh(L)
-x, y = vecs[:,1], vecs[:,2]
-spectral_coordinates = {i: (x[i], y[i]) for i in range(MST_mat.shape[0])}
+x, y = vecs[:,0], vecs[:,1]
+graph_coordinates = {i: (x[i], y[i]) for i in range(MST_mat.shape[0])}
+
+# compute spring layout (3 minutes run time), but looks very messy
+#graph_coordinates = nx.spring_layout(G)
 
 # build list of edges and nodes
 edge_x,edge_y = [],[]
 for edge in G.edges():
-    x0, y0 = spectral_coordinates[edge[0]]
-    x1, y1 = spectral_coordinates[edge[1]]
+    x0, y0 = graph_coordinates[edge[0]]
+    x1, y1 = graph_coordinates[edge[1]]
     edge_x.extend([x0, x1, None])
     edge_y.extend([y0, y1, None])
 
 node_x,node_y = [],[]
 for node in G.nodes():
-    x, y = spectral_coordinates[node]
+    x, y = graph_coordinates[node]
     node_x.append(x)
     node_y.append(y)
 
