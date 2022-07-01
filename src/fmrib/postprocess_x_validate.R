b0b71b7e0a7a44783a71cdde6e401d76917fb26e
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-07-01 01:16:08 +0100

    tested full pipeline of X validation, working fine with updated validation

--- src/fmrib/postprocess_x_validate.R
+++ src/fmrib/postprocess_x_validate.R
@@ -2,7 +2,7 @@
 path = "NeuroPM/io/"
 
 # load pseudotime scores
-psuedotimes = read.csv(file.path(path,"pseudotimes.csv"), header=TRUE)
+pseudotimes_full = read.csv(file.path(path,"pseudotimes.csv"), header=TRUE)
 
 # list X validation files
 X_val_files = list.files(path)
@@ -12,10 +12,10 @@ X_val_files = X_val_files[grepl("pseudotimes_fold", X_val_files)]
 n_folds = length(X_val_files)
 
 # initialize matrix of scores
-pseudotime_mat = matrix(NA, nrow = nrow(psuedotimes), ncol = n_folds)
+pseudotime_mat = matrix(NA, nrow = nrow(pseudotimes_full), ncol = n_folds)
 
 # define row indices for each fold to place into X validation matrix
-ind = floor(seq(from = 1, to = nrow(psuedotimes), length = n_folds + 1))
+ind = floor(seq(from = 1, to = nrow(pseudotimes_full), length = n_folds + 1))
 
 # load pseudotime scores for each fold
 for (i in 1:n_folds) {
@@ -41,7 +41,22 @@ mean_sd = apply(pseudotime_mat, 1, function(x) sd(x, na.rm = TRUE))
 mean_sd_confint = unname(quantile(mean_sd, c(0.025, 0.975)))
 
 # display outputs (raw values)
-print(sprintf("----------- Evaluating Stability of Disease Scores (Raw Values)"))
+print(sprintf("\n----------- Evaluating Stability of Disease Scores (Between Folds)"))
+print(sprintf("Root Mean Squared Error is: %0.2f +- %0.2f [%0.2f, %0.2f]",
+              mean(mean_rms), sd(mean_rms), mean_rms_confint[1], mean_rms_confint[2]))
+print(sprintf("Mean Standard Deviation is: %0.2f +- %0.2f [%0.2f, %0.2f]",
+              mean(mean_sd), sd(mean_sd), mean_sd_confint[1], mean_sd_confint[2]))
+
+# root mean squared variation from complete run
+mean_rms = sapply(1:nrow(pseudotimes_full), function(i) sqrt(mean((pseudotime_mat[i,] - pseudotimes_full$global_pseudotimes[i])^2, na.rm = TRUE)))
+mean_rms_confint = unname(quantile(mean_rms, c(0.025, 0.975)))
+
+# standard deviation variation
+mean_sd = sapply(1:nrow(pseudotimes_full), function(i) sd(pseudotime_mat[i,] - pseudotimes_full$global_pseudotimes[i], na.rm = TRUE))
+mean_sd_confint = unname(quantile(mean_sd, c(0.025, 0.975)))
+
+# display outputs (raw values)
+print(sprintf("\n----------- Evaluating Stability of Disease Scores (Against Full Run)"))
 print(sprintf("Root Mean Squared Error is: %0.2f +- %0.2f [%0.2f, %0.2f]",
               mean(mean_rms), sd(mean_rms), mean_rms_confint[1], mean_rms_confint[2]))
 print(sprintf("Mean Standard Deviation is: %0.2f +- %0.2f [%0.2f, %0.2f]",
