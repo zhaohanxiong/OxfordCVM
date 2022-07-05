ee1e3cdcb39382897ca79deede5826cde8dcdccf
zhaohan xiong <zhaohanxiong@github.com>
2022-07-05 23:43:54 +1200

    successfully deployed output dataframes to AWS

--- src/fmrib/deploy_remote_db.py
+++ src/fmrib/deploy_remote_db.py
@@ -1,26 +1,41 @@
 import os
 import json
+import boto3
 import sqlalchemy
 import pandas as pd
 
-# source path & set the current working directory
-path = "NeuroPM/io/"
+# source path of data and switch to this path
+path = "src/fmrib/NeuroPM/io/"
+os.chdir(path)
 
-# credentials
-aws_cred = json.load(open("../../../keys/aws/postgresql.json"))
+# AWS RDS credentials
+aws_cred = json.load(open("../../../../../keys/aws/postgresql.json"))
 
-# set up connection to database
+# set up connection to AWS RDS postgresql
 url = 'postgresql+psycopg2://' + aws_cred['user'] + ':' + aws_cred['passw'] + \
        '@' + aws_cred['host'] + ':' + aws_cred['port'] + '/' + aws_cred['database']
 engine = sqlalchemy.create_engine(url, echo = False)
 
 # load data frames to deploy
-pseudotimes = pd.read_csv(os.path.join(path, "pseudotimes.csv"), index_col = False)
-ukb_varnames = pd.read_csv(os.path.join(path, "ukb_varnames.csv"), index_col = False)
+pseudotimes = pd.read_csv("pseudotimes.csv", index_col = False)
+ukb_varnames = pd.read_csv("ukb_varnames.csv", index_col = False)
 
 # deploy data frames to to AWS
 pseudotimes.to_sql(name = "psuedotimes", con = engine, if_exists = "replace")
 ukb_varnames.to_sql(name = "ukb_varnames", con = engine, if_exists = "replace")
+
+# AWS S3 credentials
+aws_cred = pd.read_csv("../../../../../keys/aws/s3.csv")
+
+# set up connection to AWS S3
+s3_client = boto3.client('s3', aws_access_key_id = aws_cred['Access key ID'][0], \
+                               aws_secret_access_key = aws_cred['Secret access key'][0])
+
+# load large ukb variable dataframe (over 1000 columns)
+ukb_num = pd.read_csv("ukb_num.csv", index_col = False)
+
+# deploy to AWS S3
+response = s3_client.upload_file("ukb_num.csv", "biobank-s3", "ukb_num.csv")
 
 '''
 # query tool
