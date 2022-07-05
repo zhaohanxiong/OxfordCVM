2bedfb18f339cc925762602b9c4c68b2bb40ebd4
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-07-05 13:22:10 +0100

    refactored shell scripts

--- src/fmrib/run_python.sh
+++ src/fmrib/run_python.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# submit neuroPM model training to cluster
-fsl_sub -q bigmem.q ./run_all.sh
-
-# submit cross validation (if needed)
-#fsl_sub -q bigmem.q ./run_x_validate.sh
-
 # run python trajectory visualization/computation (in virtual env)
 /home/fs0/winokl/zxiong/env/bin/python postprocess_traj.py
 
