343b90802f51c0d62588cfb52240afaad0af530d
zhaohan xiong <zhaohanxiong@github.com>
2022-07-05 22:11:31 +1200

    added first implementation of deploying dataframe to aws, not working for large dataframe ukb_num

--- src/fmrib/deploy_remote_db.py
+++ src/fmrib/deploy_remote_db.py
@@ -1,31 +1,41 @@
-# https://towardsdatascience.com/work-with-sql-in-python-using-sqlalchemy-and-pandas-cd7693def708
-# https://www.postgresqltutorial.com/postgresql-python/connect/
-# https://alexcodes.medium.com/upload-csv-files-to-postgresql-aws-rds-using-pythons-psycopg2-613992fcd7b
-
 import os
+import json
 import sqlalchemy
-from sqlalchemy.types import Integer, Text, String, DateTime
 import pandas as pd
 
 # source path & set the current working directory
 path = "src/fmrib/NeuroPM/io/"
 
+# credentials
+aws_cred = json.load(open("../keys/aws/postgresql.json"))
+
+# set up connection to database
+url = 'postgresql+psycopg2://' + aws_cred['user'] + ':' + aws_cred['passw'] + \
+       '@' + aws_cred['host'] + ':' + aws_cred['port'] + '/' + aws_cred['database']
+engine = sqlalchemy.create_engine(url, echo = False)
+
 # load data frames to deploy
-pseudotimes = pd.read_csv(os.path.join(path, "pseudotimes.csv"), index_col=False)
-ukb_num = pd.read_csv(os.path.join(path, "ukb_num.csv"), index_col=False)
+pseudotimes = pd.read_csv(os.path.join(path, "pseudotimes.csv"), index_col = False)
+ukb_varnames = pd.read_csv(os.path.join(path, "ukb_varnames.csv"), index_col = False)
 
-# create connection to database
-url = 'postgresql+psycopg2://username:password@host:port/database'
-engine = sqlalchemy.create_engine(url)
+# deploy data frames to to AWS
+pseudotimes.to_sql(name = "psuedotimes", con = engine, if_exists = "replace")
+ukb_varnames.to_sql(name = "ukb_varnames", con = engine, if_exists = "replace")
 
-# write to new table
-if True:
-    pseudotimes.to_sql("biobank", con = engine, if_exists = "replace", schema = "ukb", 
-                       index = True, chucksize = 1000,
-                       dytype = {})
-    
+'''
+# query tool
+# initialize connected database
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
 
-# convert to SQL
+# create pointer to database table
+table = sqlalchemy.Table('psuedotimes', metadata, autoload = True, \
+                                        autoload_with = engine)
 
-
-# deploy to AWS
+# query table
+sql_query = sqlalchemy.select([table.columns.patid, \
+                               table.columns.global_pseudotimes]). \
+                where(sqlalchemy.and_(table.columns.bp_group == 2, \
+                                      table.columns.global_pseudotimes > 0.8))
+query_result = connection.execute(sql_query).fetchall()
+'''
