edb85be8c8e5bbc1d39cf5d77e058810917b1c7c
zhaohan xiong <zhaohanxiong@github.com>
2022-07-13 01:06:53 +1200

    added further evaluation to KNN pred file

--- src/fmrib/postprocess_eval_model.R
+++ src/fmrib/postprocess_eval_model.R
@@ -31,14 +31,14 @@ g2_box = unname(c(quantile(g2, 0.25), quantile(g2, 0.75))) # between
 g3_box = unname(c(quantile(g3, 0.25), quantile(g3, 0.75))) # disease
 
 # display results for quantifying IQR overlap
-sprintf(paste0("Overlap in IQR of Background vs Between is ",
-               "%0.1f%% (Background) %0.1f%% of (Between)"),
-        (g1_box[2] - g2_box[1]) / diff(g1_box) * 100,
-        (g1_box[2] - g2_box[1]) / diff(g2_box) * 100)
-sprintf(paste0("Overlap in IQR of Boxes Between vs Disease is ",
-               "%0.1f%% (Between) %0.1f%% (Disease)"),
-        (g2_box[2] - g3_box[1]) / diff(g2_box) * 100,
-        (g2_box[2] - g3_box[1]) / diff(g3_box) * 100)
+print(sprintf(paste0("Overlap in IQR of Background vs Between is ",
+                     "%0.1f%% (Background) %0.1f%% of (Between)"),
+              (g1_box[2] - g2_box[1]) / diff(g1_box) * 100,
+              (g1_box[2] - g2_box[1]) / diff(g2_box) * 100))
+print(sprintf(paste0("Overlap in IQR of Boxes Between vs Disease is ",
+                     "%0.1f%% (Between) %0.1f%% (Disease)"),
+              (g2_box[2] - g3_box[1]) / diff(g2_box) * 100,
+              (g2_box[2] - g3_box[1]) / diff(g3_box) * 100))
 
 # --------------------------------------------------------------------------------------------
 # Quantifying Group Overlap Evaluation
@@ -57,12 +57,12 @@ background_q = 1 - n_background_overlap/length(sample_background)
 disease_q = 1 - n_disease_overlap/length(sample_disease)
 
 # display results for quantifying distribution overlap
-sprintf("Overlapping Interval of Scores is %0.1f%% of the entire range (%0.3f to %0.3f)", 
-        overlap * 100, min(sample_disease), max(sample_background))
-sprintf("%% of Samples in the Background Group with Non-Overlapping Scores is %0.1f%%",
-        background_q * 100)
-sprintf("%% of Samples in the Disease Group with Non-Overlapping Scores is %0.1f%%",
-        disease_q * 100)
+print(sprintf("Overlapping Interval of Scores is %0.1f%% of the entire range (%0.3f to %0.3f)", 
+        overlap * 100, min(sample_disease), max(sample_background)))
+print(sprintf("%% of Samples in the Background Group with Non-Overlapping Scores is %0.1f%%",
+        background_q * 100))
+print(sprintf("%% of Samples in the Disease Group with Non-Overlapping Scores is %0.1f%%",
+        disease_q * 100))
 
 # --------------------------------------------------------------------------------------------
 # AUROC Evaluation
@@ -72,27 +72,21 @@ y_pred = psuedotimes$global_pseudotimes[psuedotimes$bp_group != "Between"]
 y_true = ifelse(psuedotimes$bp_group[psuedotimes$bp_group != "Between"] == "Background", 0, 1)
 
 # compute FPR (false positive rate) and TPR (true positive rate) for different thresholds
-intervals = c(seq(0, 0.1, by = 0.01), seq(0.2, 1, by = 0.1))
+intervals = seq(0, 1, by = 0.001)
 threshold_mat = sapply(intervals, function(thres) ifelse(y_pred > thres, 1, 0))
 fpr = apply(threshold_mat, 2, function(x) 
-                                sum(x == 1 & y_true == 0) / 
-                                  (sum(x == 1 & y_true == 0) + sum(x == 0 & y_true == 0)))
+                sum(x == 1 & y_true == 0) / 
+                  (sum(x == 1 & y_true == 0) + sum(x == 0 & y_true == 0)))
 tpr = apply(threshold_mat, 2, function(x)
-                                sum(x == 1 & y_true == 1) / 
-                                  (sum(x == 1 & y_true == 1) + sum(x == 0 & y_true == 1)))
+                  sum(x == 1 & y_true == 1) / 
+                    (sum(x == 1 & y_true == 1) + sum(x == 0 & y_true == 1)))
 
 # compute AUC (using sum of trapeziums)
 auc = sum((tpr[1:(length(intervals) - 1)] + tpr[2:length(intervals)]) * diff(1 - fpr) / 2)
 
 # display output
-sprintf("AUC is %0.5f when using %0.0f Logarithmic Intervals", auc, length(intervals))
-
-# create a data frame to view variation of threshold to fpr/tpr
-if (FALSE) {
-  View(data.frame(threshold = intervals,
-                  specificity = 1 - fpr,
-                  sensitivity = tpr))
-}
+print(sprintf("AUC is %0.5f when using %0.0f Logarithmic Intervals", 
+              auc, length(intervals)))
 
 # plot AUROC (area under receiver operating characteristic curve)
 if (FALSE) {
@@ -113,3 +107,16 @@ if (FALSE) {
         lwd = 3, col = "purple")
 
 }
+
+# create a data frame to view variation of threshold to fpr/tpr
+if (FALSE) {
+  View(data.frame(threshold = intervals,
+                  specificity = 1 - fpr,
+                  sensitivity = tpr))
+}
+
+# perform optimization to find optimal threshold, x = threshold
+ind = which.max(1 - fpr + tpr)
+print(sprintf(paste0("Optimal Threshold at %0.3f (Sensitivty = %0.1f%%, ",
+                     "Specificity = %0.1f%%)"), 
+              intervals[ind], tpr[ind] * 100, (1 - fpr)[ind] * 100))
--- src/fmrib/postprocess_x_validate_KNNpred.R
+++ src/fmrib/postprocess_x_validate_KNNpred.R
@@ -1,13 +1,18 @@
 library(data.table)
 
 # load outputs from NeuroPM
-path = "NeuroPM/io"
+path = "C:/Users/86155/Desktop/11_10_fold_X_validation" #"NeuroPM/io"
+
+# set K (for KNN)
+K = 10
 
 # load pseudotime scores
 pseudotimes_full = read.csv(file.path(path,"pseudotimes.csv"), header=TRUE)
+pseudotimes_full$err = NA
+pseudotimes_full$knn_dist = NA
 
 # load ukb raw variables
-ukb_df = data.frame(fread(file.path(path, "ukb_num.csv"), header=TRUE))
+ukb_df = data.frame(fread(file.path(path, "ukb_num_norm.csv"), header=TRUE))
 
 # list X validation files
 X_val_files = list.files(path)
@@ -19,15 +24,12 @@ n_folds = length(X_val_files)
 # define row indices for each fold to place into X validation matrix
 ind = floor(seq(from = 1, to = nrow(pseudotimes_full), length = n_folds + 1))
 
-# store outputs
-err_all = rep(NA, n_folds)
-acc_all = rep(NA, n_folds)
-
 # load pseudotime scores for each fold
 for (i in 1:n_folds) {
   
   # load file from i'th fold
-  pseudotimes = read.csv(file.path(path, paste0("pseudotimes_fold",i,".csv")), header=TRUE)
+  pseudotimes = read.csv(file.path(path, paste0("pseudotimes_fold", i, 
+                                                ".csv")), header=TRUE)
   
   # create indices for indexing
   ind_i = ind[i]:(ind[i + 1] - (i != n_folds))
@@ -41,43 +43,63 @@ for (i in 1:n_folds) {
 
   # create dataframe of ground truth and predictions for disease score
   eval = data.frame(gt = pseudotimes_full$global_pseudotimes[ind_i],
-                    pred = NA,
+                    pred = 0,
+                    knn_dist = 0,
                     group = pseudotimes_full$bp_group[ind_i])
-  
-  # set K (for KNN)
-  K = 10
   
   # perform KNN to infer disease score
   for (j in 1:nrow(pred_data)) {
-
+    
     # compute distance with each row
     dist_j = colSums(abs(t(ref_data) - pred_data[j,]), na.rm = TRUE)
     
     # compute KNN and prediction
     sorted_ind = order(dist_j)[1:K]
     eval$pred[j] = mean(ref_label[sorted_ind])
+    eval$knn_dist[j] = mean(sort(dist_j)[1:K])
     
   }
+
+  # compute err
+  eval$err = sqrt((eval$pred - eval$gt)**2)
   
-  # compute err and accuracy
-  eval$mse = sqrt((eval$pred - eval$gt)**2)
-  eval$acc = abs(eval$pred - eval$gt) / eval$gt * 100
-  err = mean(eval$mse)
-  acc = mean(eval$acc)
-  
-  # append 
-  err_all[i] = err
-  acc_all[i] = acc
+  # compute sensitivity/specificity
+  y_true = ifelse(pseudotimes_full$bp_group[ind_i][pseudotimes_full$
+                                                bp_group[ind_i] != 0] == 1,
+                  0, 1)
+  intervals = seq(0, 1, by = 0.001)
+  threshold_mat = sapply(intervals, function(thres)
+                                            ifelse(eval$pred > thres, 1, 0))
+  fpr = apply(threshold_mat, 2, function(x) 
+                sum(x == 1 & y_true == 0) / 
+                  (sum(x == 1 & y_true == 0) + sum(x == 0 & y_true == 0)))
+  tpr = apply(threshold_mat, 2, function(x)
+                sum(x == 1 & y_true == 1) / 
+                  (sum(x == 1 & y_true == 1) + sum(x == 0 & y_true == 1)))
+  ind = which.max(1 - fpr + tpr)
   
   # display summaries, also by group
-  sprintf(paste0("Fold %.0f: Predictions were %0.3f from the Ground",
-                 " Truth (Accuracy = %0.1f%%, N = %.0f)"),
-          i, err, acc, length(ind_i))
-  sprintf("Mean by Group:")
-  print(aggregate(eval$mse, list(eval$group), mean))
+  print(sprintf("------------------------------ Evaluating Fold %.0f", i))
+  print(sprintf(paste0("Predictions were %0.3f from the Ground",
+                       " Truth (N = %.0f)"),
+                mean(eval$err), length(ind_i)))
+  print(sprintf("Mean by Group:"))
+  print(aggregate(eval[, c("err", "knn_dist")], list(eval$group), mean))
+  print(sprintf(paste0("Optimal Threshold at %0.3f (Sensitivty = %0.1f%%, ",
+                       "Specificity = %0.1f%%)"), 
+                intervals[ind], tpr[ind] * 100, (1 - fpr)[ind] * 100))
+  
+  # append
+  pseudotimes_full$err[ind_i] = eval$err
+  pseudotimes_full$knn_dist[ind_i] = eval$knn_dist
   
 }
 
-sprintf(paste0("Overall: %.0f-Fold X-Validation Results in an ",
-               "Average Error of %0.2f (Accuracy = %0.1f%%)"),
-        n_folds, mean(err_all), mean(acc_all))
+# display overall results
+print(sprintf(paste0("Overall: %.0f-Fold X-Validation Results in an ",
+                     "Average Error of %0.3f"),
+              n_folds, mean(pseudotimes_full$err)))
+print(sprintf("Mean by Group:"))
+print(aggregate(pseudotimes_full[, c("err", "knn_dist")],
+                list(pseudotimes_full$bp_group), 
+                function(x) mean(x, na.rm = TRUE)))
