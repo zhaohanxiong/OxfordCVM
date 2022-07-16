2922a474a886b816cd990be521d52bfad170b905
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-07-16 06:37:33 +0100

    now writing matlab intermediate files but have option to remove them if needed

--- src/fmrib/NeuroPM/compile_NeuroPM.sh
+++ src/fmrib/NeuroPM/compile_NeuroPM.sh
@@ -8,3 +8,6 @@ mcc -m run_NeuroPM.m -a ./cTI-codes/
 
 # remove useless output files
 rm mccExcludedFiles.log readme.txt requiredMCRProducts.txt
+
+# remove output matlab files if dont need (as high storage usage)
+#rm io/all.mat io/cPCA_interm.mat
