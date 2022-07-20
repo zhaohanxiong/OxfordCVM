3b7d97c4901c4854f7170359240ab5eefd2b9ad9
zhaohan xiong <zhaohanxiong@github.com>
2022-07-20 17:55:06 +0100

    set up directory for tf-serving

---
+++ src/Deploy_ML/tf_serving_container/test_tf_serving_predict.py
@@ -0,0 +1,15 @@
+import json
+import requests
+import pandas as pd
+
+# load test data
+sample = pd.read_csv("../sample_disease.csv").fillna(0).to_numpy()
+
+# set request
+url = 'http://localhost:8501/v1/IG/default:predict'
+data = json.dumps({"instances": sample.tolist()})
+headers = {"content-type": "application/json"}
+json_response = requests.post(url, data = data, headers = headers)
+predictions = json.loads(json_response.text)['predictions']
+
+print(predictions)
