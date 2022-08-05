b4636b5245d6290e27cfc02dafdec926d8180f6e
zhaohan xiong <zhaohanxiong@github.com>
2022-08-05 16:41:59 +0100

    wrote test case for cti model

--- src/Deploy_ML/test_load_keras_model.py
+++ src/Deploy_ML/test_load_keras_model.py
@@ -1,13 +1,32 @@
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-# load model
-cTI_model = tf.keras.models.load_model("tf_serving_container/saved_models/1/")
+def test_cti_model():
 
-# load test data
-sample = pd.read_csv("../fmrib/NeuroPM/io/sample_test_data/sample_background.csv").fillna(0).to_numpy()
+    # load model
+    cTI_model = tf.keras.models.load_model("tf_serving_container/saved_models/1/")
 
-# make inference
-pred_out = cTI_model.predict(sample)
+    # Arrange
+    # load test data
+    #test_sample1 = pd.read_csv("../fmrib/NeuroPM/io/ukb_num_norm.csv").iloc[0].fillna(0).to_numpy()
+    test_sample = pd.read_csv("../fmrib/NeuroPM/io/ukb_num_norm.csv").fillna(0).to_numpy()
 
-print(pred_out)
+    # load labels
+    test_label = pd.read_csv("../fmrib/NeuroPM/io/pseudotimes.csv")
+
+    # Action
+    # make inference for each row
+    pred = []
+    for i in range(test_sample.shape[0]):
+        pred.append(cTI_model.predict(test_sample[None, i, :])[0])
+
+    gt, pred = test_label["global_pseudotimes"].to_numpy(), np.array(pred)
+
+    # compute accuracy
+    rmse = np.mean(np.sqrt((gt - pred)**2))
+
+    # Assert
+    assert rmse < 0.1
+
+test_cti_model()
