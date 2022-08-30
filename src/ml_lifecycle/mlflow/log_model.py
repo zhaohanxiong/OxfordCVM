fac9e490312f5869326a5a045db35e83482301f2
Zhao Xiong <zhaohanx@hotmail.com>
2022-08-30 16:08:19 +0100

    added tracking

--- src/ml_lifecycle/mlflow/log_model.py
+++ src/ml_lifecycle/mlflow/log_model.py
@@ -7,13 +7,17 @@ import tensorflow as tf
 cTI_model = tf.keras.models.load_model("../tf_serving/saved_models/2/")
 
 # set random seed
-rs = 123
+rs = 12345
 
 # load data & labels
 test_sample = pd.read_csv("../../fmrib/NeuroPM/io/ukb_num_norm.csv").fillna(0)
 test_sample = test_sample.sample(n = 100, random_state = rs).to_numpy()
 test_label = pd.read_csv("../../fmrib/NeuroPM/io/pseudotimes.csv")
 test_label = test_label.sample(n = 100, random_state = rs)
+
+# initialize mlflow session, this can be used for remote tracking too
+mlflow.set_tracking_uri("http://localhost:5000/")
+mlflow.set_experiment("cti_predict")
 
 # start mlflow session for tracking
 with mlflow.start_run():
@@ -31,4 +35,6 @@ with mlflow.start_run():
 
     # log metric to mlflow server manually
     # automatic logging can also be performed: https://www.mlflow.org/docs/latest/tracking.html#tensorflow-and-keras
-    mlflow.log_metric(key = "RMSE", value = rmse)
+    mlflow.set_tag(key = "eval_model", value = "0")
+    mlflow.log_metric(key = "RMSE", value = rmse, step = 1)
+    mlflow.log_param(key = "ver", value = "2")
