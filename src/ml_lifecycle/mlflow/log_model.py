d553fb00c462ce166e316967d127bb34c6339041
Zhao Xiong <zhaohanx@hotmail.com>
2022-08-31 15:42:09 +0100

    added db logging with model tracking

--- src/ml_lifecycle/mlflow/log_model.py
+++ src/ml_lifecycle/mlflow/log_model.py
@@ -16,12 +16,15 @@ args = parser.parse_args(sys.argv[1:])
 
 # load data & labels
 test_sample = pd.read_csv("../../fmrib/NeuroPM/io/ukb_num_norm.csv").fillna(0)
-test_sample = test_sample.sample(n = args.random_n, random_state = args.random_seed).to_numpy()
+test_sample = test_sample.sample(n = args.random_n, 
+                                 random_state = args.random_seed).to_numpy()
 test_label = pd.read_csv("../../fmrib/NeuroPM/io/pseudotimes.csv")
-test_label = test_label.sample(n = args.random_n, random_state = args.random_seed)
+test_label = test_label.sample(n = args.random_n,
+                               random_state = args.random_seed)
 
 # initialize mlflow session, this can be used for remote tracking too
-mlflow.set_tracking_uri("http://localhost:5000/")
+mlflow.set_tracking_uri("mlruns")
+mlflow.set_registry_uri("sqlite:///mlruns.db")
 mlflow.set_experiment("cti_predict")
 
 # start mlflow session for tracking
@@ -36,7 +39,14 @@ with mlflow.start_run(run_name = "test run"):
     gt, pred = test_label["global_pseudotimes"].to_numpy(), np.array(pred)
 
     # evaluate accuracy of predictions against ground truths
-    rmse = np.mean(np.sqrt((gt - pred)**2))
+    rmse = np.sqrt((gt - pred)**2)
+
+    # evaluate by group
+    bp_group = test_label["bp_group"].to_numpy()
+    rmse_0 = np.mean(rmse[bp_group == 1])
+    rmse_1 = np.mean(rmse[bp_group == 0])
+    rmse_2 = np.mean(rmse[bp_group == 2])
+    rmse = np.mean(rmse)
 
     # log model
     mlflow.keras.log_model(keras_model = cTI_model,
@@ -45,6 +55,7 @@ with mlflow.start_run(run_name = "test run"):
 
     # log metric to mlflow server manually
     # automatic logging can also be performed: https://www.mlflow.org/docs/latest/tracking.html#tensorflow-and-keras
-    mlflow.set_tag(key = "eval_model", value = "0")
-    mlflow.log_metric(key = "RMSE", value = rmse, step = 1)
+    mlflow.set_tags({"version": "0.0", "model": "keras"})
+    mlflow.log_metrics({"RMSE": rmse, "RMSE_background": rmse_0, 
+                        "RMSE_between": rmse_1, "RMSE_disease": rmse_2})
     mlflow.log_param(key = "ver", value = "2")
