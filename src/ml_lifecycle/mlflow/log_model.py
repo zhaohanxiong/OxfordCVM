2bbf426bd8a75784d04bd673ecad18448edf31e6
Zhao Xiong <zhaohanx@hotmail.com>
2022-08-31 20:22:28 +0100

    added results visualization

--- src/ml_lifecycle/mlflow/log_model.py
+++ src/ml_lifecycle/mlflow/log_model.py
@@ -72,7 +72,7 @@ with mlflow.start_run(run_name = "test run") as run:
 
     # log metric to mlflow server manually
     # automatic logging can also be performed: https://www.mlflow.org/docs/latest/tracking.html#tensorflow-and-keras
-    mlflow.set_tags({"version": "0.0", "model": "keras"})
+    mlflow.set_tags({"experiment version": "0.0", "model flavour": "keras"})
     mlflow.log_metrics({"RMSE": rmse, "RMSE_background": rmse_0, 
                         "RMSE_between": rmse_1, "RMSE_disease": rmse_2})
     mlflow.log_params({"n_rows": test_label.shape[0]})
@@ -97,8 +97,8 @@ with mlflow.start_run(run_name = "test run") as run:
     #client.delete_registered_model(name = "registered_model_name")
 
     # store output visualization for results
-    plot_path = "./mlruns/" + experiment_id + "/" + run.info.run_id + "artifacts/keras_models"
+    plot_path = "./mlruns/" + experiment_id + "/" + run.info.run_id + "/artifacts/keras_models"
     
     plt = sns.boxplot(x = 'bp_group', y = 'global_pseudotimes', data = test_label)
     fig = plt.get_figure()
-    fig.savefig(plot_path + "/disease score distribution") 
+    fig.savefig(plot_path + "/disease score distribution.png") 
