9547995230589cc65fab714f590efe7430b6db6b
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-09-03 16:56:56 +0100

    added

--- src/run_all.sh
+++ src/run_all.sh
@@ -8,7 +8,7 @@ cd ./fmrib
 #Rscript ukb_whole_data_subset.R
 
 # run R preprocessing script, writes to NeuroPM/io directory
-#Rscript preprocess_data_preparation.R
+Rscript preprocess_data_preparation.R
 Rscript preprocess_feature_selection.R
 
 # compile matlab script (only if there were code changes)
