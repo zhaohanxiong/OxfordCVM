956eeb2de370e0416196e6ac3ef2d8eea3aa2e1c
Winok Lapidaire <winokl@jalapeno.cluster.fmrib.ox.ac.uk>
2022-09-14 15:31:11 +0100

    added new filtering criteria to entire UKB

--- src/fmrib/preprocess_utils.R
+++ src/fmrib/preprocess_utils.R
@@ -28,8 +28,8 @@ load_raw_ukb_patient_dataset = function(path_ukb_data, path_ukb_vars) {
   # set blood pressure variables if not present already
   if (length(grep("BPSys", colnames(df))) == 0) {
 
-    df[["BPSys-2.0"]] = ifelse(is.na(df[["4080-2.0"]]), df[["4080-0.0"]], df[["4080-2.0"]])
-    df[["BPDia-2.0"]] = ifelse(is.na(df[["4079-2.0"]]), df[["4079-0.0"]], df[["4079-2.0"]])
+    df[["BPSys-2.0"]] = ifelse(is.na(df[["4080-2.0"]]), df[["93-2.0"]], df[["4080-2.0"]])
+    df[["BPDia-2.0"]] = ifelse(is.na(df[["4079-2.0"]]), df[["94-2.0"]], df[["4079-2.0"]])
 
   }
   
