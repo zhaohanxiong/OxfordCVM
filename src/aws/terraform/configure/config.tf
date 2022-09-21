8bae7763ece8d6ef4876295950f86992ee90e9fc
Zhao Xiong <zhaohanx@hotmail.com>
2022-09-21 16:37:40 +0100

    added new block for configuring aws env

---
+++ src/aws/terraform/configure/config.tf
@@ -0,0 +1,98 @@
+# store the state of the terraform project in AWS S3
+# comment out to store locally
+# terraform {
+#     backend "s3" {
+#         bucket = "cti-ukb-tf-state"
+#         key    = "state.tfstate"
+#     }
+# }
+
+# define platform (when running with aws CLI)
+provider "aws" {
+    profile = "default"
+    region  = "us-east-1"
+}
+
+data "aws_availability_zones" "available_zones" {
+    state = "available"
+}
+
+# configure virtual private cloud to create isolated virtual network 
+resource "aws_vpc" "vpc" {
+    cidr_block = "10.0.0.0/24"
+    enable_dns_support   = true
+    enable_dns_hostnames = true
+    tags = {
+        Name = "Terraform VPC"
+    }
+}
+
+# configure communication between instances in VPC and internet
+resource "aws_internet_gateway" "internet_gateway" {
+    vpc_id = aws_vpc.vpc.id
+}
+
+# configure and create sub network
+resource "aws_subnet" "pub_subnet" {
+    vpc_id     = aws_vpc.vpc.id
+    cidr_block = "10.0.0.16/28"
+}
+
+# configure where network traffic from subnets are directed
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.internet_gateway.id
+    }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+    subnet_id      = aws_subnet.pub_subnet.id
+    route_table_id = aws_route_table.public.id
+}
+
+# configure security groups to restrict access
+resource "aws_security_group" "ecs_sg" {
+    vpc_id = aws_vpc.vpc.id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 65535
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "rds_sg" {
+    vpc_id = aws_vpc.vpc.id
+
+    ingress {
+        protocol        = "tcp"
+        from_port       = 5432
+        to_port         = 5432
+        cidr_blocks     = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.ecs_sg.id]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 65535
+        protocol        = "tcp"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
