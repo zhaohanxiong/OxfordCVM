ae0557b63ef44f1c4e9159d8546b654aee3fe26d
Zhao Xiong <zhaohanx@hotmail.com>
2022-09-22 19:20:18 +0100

    updated luanch config

--- src/aws/terraform/compute.tf
+++ src/aws/terraform/compute.tf
@@ -34,8 +34,11 @@ resource "aws_launch_configuration" "ecs_launch_config" {
     image_id             = "ami-05fa00d4c63e32376"
     iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
     security_groups      = [aws_security_group.ecs_sg.id]
-    user_data            = "#!/bin/bash\necho ECS_CLUSTER=default >> /etc/ecs/ecs.config"
+    user_data            = "#!/bin/bash\necho ECS_CLUSTER=cti-cluster >> /etc/ecs/ecs.config"
     instance_type        = "t2.micro"
+    lifecycle {
+        create_before_destroy = true
+    }
 }
 
 # configure autoscaling group containing a collection of EC2
@@ -54,7 +57,7 @@ resource "aws_autoscaling_group" "failure_analysis_ecs_asg" {
 
 # create a cluster
 resource "aws_ecs_cluster" "ecs_cluster" {
-    name  = "default"
+    name  = "cti-cluster"
 }
 
 # create a task definition
