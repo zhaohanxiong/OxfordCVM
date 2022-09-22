a7a3ec64a40f6b2238cf083d22afbf012c468803
Zhao Xiong <zhaohanx@hotmail.com>
2022-09-22 12:55:48 +0100

    RDS still not working :(

--- src/aws/terraform/configure.tf
+++ src/aws/terraform/configure.tf
@@ -77,3 +77,28 @@ resource "aws_security_group" "rds_sg" {
         cidr_blocks     = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "ecs_sg" {
+    vpc_id = aws_vpc.vpc.id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
