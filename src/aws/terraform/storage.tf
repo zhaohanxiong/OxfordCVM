304732afce221402412c97a921168a54bb8abc5d
Zhao Xiong <zhaohanx@hotmail.com>
2022-09-22 15:43:45 +0100

    restart again FFFFFFFF

--- src/aws/terraform/storage.tf
+++ src/aws/terraform/storage.tf
@@ -29,9 +29,8 @@ resource "aws_db_instance" "rds_postgresql_name" {
     allocated_storage                   = 10
     port                                = 5432
     iam_database_authentication_enabled = false
-    #db_subnet_group_name                = aws_db_subnet_group.db_subnet_group.id
-    #vpc_security_group_ids              = [aws_security_group.rds_sg.id]
-    final_snapshot_identifier           = "ukb-db-final"
+    db_subnet_group_name                = aws_db_subnet_group.db_subnet_group.id
+    vpc_security_group_ids              = [aws_security_group.rds_sg.id]
 }
 
 # output various parameters associated with the RDS instance
