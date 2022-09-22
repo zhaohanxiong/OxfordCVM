508e6f8bad537dd1cfd9df4cebbfddb67e14a9b5
Zhao Xiong <zhaohanx@hotmail.com>
2022-09-22 14:23:36 +0100

    removed ec2 sg

--- src/aws/terraform/configure.tf
+++ src/aws/terraform/configure.tf
@@ -61,44 +61,18 @@ resource "aws_route_table_association" "route_table_association" {
 
 # configure security groups to restrict access
 resource "aws_security_group" "rds_sg" {
+    name = "rds_sg"
     vpc_id = aws_vpc.vpc.id
-
     ingress {
         protocol        = "tcp"
         from_port       = 5432
         to_port         = 5432
         cidr_blocks     = ["0.0.0.0/0"]
     }
-
     egress {
         from_port       = 0
         to_port         = 0
         protocol        = "-1"
         cidr_blocks     = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_security_group" "ecs_sg" {
-    vpc_id = aws_vpc.vpc.id
-
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
     }
 }
