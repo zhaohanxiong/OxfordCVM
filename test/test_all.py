016586162f8ffade57dc79dadc248250f8410a39
zhaohan xiong <zhaohanxiong@github.com>
2022-09-24 22:35:03 +0100

    added test-all pytest

--- test/test_all.py
+++ test/test_all.py
@@ -3,11 +3,20 @@ from utils import read_test_dict
 def test_check_all_flags_shouldpass():
 
     '''
-    This function loads the test dictionary flag file and checks
-    if all the values are true, meaning all tests have passed
-    successfully in our pytest environment
+    This function loads the test dictionary flag file and checks if all
+    the values are true, meaning all tests have passed successfully in 
+    our pytest environment
     '''
     
+    # Arrange
+    # load test flag dictionary
     test_dict = read_test_dict()
 
-    return True
+    # Action
+    # loop through all keys
+    for key_group in test_dict:
+        for key, val in test_dict[key_group].items():
+
+    # Assert
+            # check if current condition is true for the group
+            assert val, "test failed: " + key_group + " (" + key + ")"
