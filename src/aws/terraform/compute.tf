c1b3eee68b6fdab19f1c0120e6731b2903b23a19
Zhao Xiong <zhaohanx@hotmail.com>
2022-09-24 18:48:30 +0100

    updated health check

--- src/aws/terraform/compute.tf
+++ src/aws/terraform/compute.tf
@@ -113,7 +113,7 @@ resource "aws_lb_target_group" "lb_target_group" {
     target_type = "ip"
     health_check {
         interval            = "300"
-        port                = "8500"
+        port                = "80"
         healthy_threshold   = "2"
         unhealthy_threshold = "2"
     }
