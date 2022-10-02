eed32b3a21a9bf4b76733d4a07083bd2e2659384
Zhao Xiong <zhaohanx@hotmail.com>
2022-10-02 15:35:52 +0100

    removed old code from repo

--- resources/src_old_code/main_tsne.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-library(tsne)
-
-path = "C:/users/zxiong/desktop/workspace vars 20% run/"
-df = fread(file.path(path, "ukb_num_norm.csv"))
-df = data.frame(df)
-
-reduce = tsne(df, max_iter = 500, k = 50)
-fwrite(data.frame(reduce), file.path(path, "tsne.csv"))
