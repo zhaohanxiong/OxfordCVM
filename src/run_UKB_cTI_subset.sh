7e0f88fff165eeae1ec1abf8cb160aff04c2956f
zhaohan xiong <zhaohanxiong@github.com>
2022-10-25 22:15:42 +0100

    updated shell script to include pytest

--- src/run_UKB_cTI_subset.sh
+++ src/run_UKB_cTI_subset.sh
@@ -18,3 +18,12 @@ nohup ./run_run_NeuroPM.sh /opt/fmrib/MATLAB/MATLAB_Compiler_Runtime/v98
 # run post-processing file organization/evaluation
 cd ..
 Rscript postprocess_eval_model.R
+
+# run test cases (in root directory)
+cd ../..
+pytest ./test/test_neuropm.py -k "test_io_R_preprocess_output_exist_shouldpass"
+pytest ./test/test_neuropm.py -k "test_io_R_ft_select_output_exist_shouldpass"
+pytest ./test/test_neuropm.py -k "test_io_R_postprocess_output_exist_shouldpas"
+pytest ./test/test_neuropm.py -k "test_io_neuropm_output_exist_shouldpass"
+pytest ./test/test_neuropm.py -k "test_io_neuropm_interm_output_exist_shouldpass"
+pytest ./test/test_neuropm.py -k "test_neuro_pm_accuracy_shouldpass"
