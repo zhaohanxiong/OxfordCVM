fec9adcef7dfd6b8f41510e0e3d28ddebe5eee88
zhaohan xiong <zhaohanxiong@github.com>
2022-11-17 17:16:57 +0000

    added sub-run too

--- src/run_UKB_cTI_subset.sh
+++ src/run_UKB_cTI_subset.sh
@@ -20,7 +20,9 @@ cd ..
 Rscript postprocess_eval_model.R
 
 # run test cases (in root directory)
-cd ../..
+cd ../../test
+python init.py
+cd ..
 pytest ./test/test_neuropm.py -k "test_io_R_preprocess_output_exist_shouldpass"
 pytest ./test/test_neuropm.py -k "test_io_R_ft_select_output_exist_shouldpass"
 pytest ./test/test_neuropm.py -k "test_io_R_postprocess_output_exist_shouldpas"
