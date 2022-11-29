dbf77eb0a2f13134f64588519215c7417e370f35
zhaohanxiong <zhaohanx@hotmail.com>
2022-11-29 00:04:22 +0000

    added contrast cov

--- src/modelling/preprocess_feature_selection.R
+++ src/modelling/preprocess_feature_selection.R
@@ -12,15 +12,10 @@ ft_norm = ft_norm[ind_rand, ]
 labels = labels[ind_rand, ]
 
 # # # compute covariance matrices
-# all
-cov_all = cov(ft_norm)
+# background + disease
+cov_all = cov(ft_norm[labels$bp_group == 1 | labels$bp_group == 2, ])
 cov_all[upper.tri(cov_all)] = NA
 diag(cov_all) = NA
-
-# background + disease
-cov_cti = cov(ft_norm[labels$bp_group == 1 | labels$bp_group == 2, ])
-cov_cti[upper.tri(cov_cti)] = NA
-diag(cov_cti) = NA
 
 # background
 cov_background = cov(ft_norm[labels$bp_group == 1, ])
@@ -29,39 +24,45 @@ diag(cov_background) = NA
 
 # disease
 cov_disease = cov(ft_norm[labels$bp_group == 2, ])
-cov_disease[upper.tri(cov_disease)] = 0
-diag(cov_disease) = 0
+cov_disease[upper.tri(cov_disease)] = NA
+diag(cov_disease) = NA
 
-# # # feature selection # note that cPCA: cov = cov_d - a * cov_b
+# contrast cov = cov_d - a * cov_b
+cov_contrast = cov(ft_norm[labels$bp_group == 2, ]) - cov(ft_norm[labels$bp_group == 1, ])
+cov_contrast[upper.tri(cov_contrast)] = NA
+diag(cov_contrast) = NA
+
+# # # feature selection
 # find high co-correlation variables
-ind_omit = unname(apply(cov_all, 1, function(x)
-                            any(abs(x) > 0.75, na.rm = TRUE)))
+ind_omit = unname(apply(cov_contrast, 1, function(x)
+                            mean(abs(x), na.rm = TRUE) < 0.1))
 
 # find brain variables
 var_brain = var_groups$ukb_var[var_groups$var_group == "Brain_MR"]
 var_brain = colnames(ft_norm) %in% var_brain
 
 # remove only brain variables
-ind_omit[!var_brain] = FALSE
+#ind_omit[!var_brain] = FALSE
 
 # only keep relevant features
 ft_norm = ft_norm[, !ind_omit]
 
-# # # shuffle labels for experimentation
-# labels$bp_group = sample(labels$bp_group, nrow(labels))
+# # # Experimentation
+# shuffle labels for experimentation
+#labels$bp_group = sample(labels$bp_group, nrow(labels))
 
 # only keep subset of background/disease for experimentation
-# n_sample = 750
-# ft_norm = rbind(ft_norm[labels$bp_group == 1, ][1:n_sample, ],
-#                 ft_norm[labels$bp_group != 1, ])
-# labels = rbind(labels[labels$bp_group == 1, ][1:n_sample, ],
-#             labels[labels$bp_group != 1, ])
+#n_sample = 500
+#ft_norm = rbind(ft_norm[labels$bp_group == 1, ][1:n_sample, ],
+#                ft_norm[labels$bp_group != 1, ])
+#labels = rbind(labels[labels$bp_group == 1, ][1:n_sample, ],
+#               labels[labels$bp_group != 1, ])
 
+# # # output
 # display messages to see in terminal
 print(sprintf("Further Filtering Complete"))
 print(sprintf("Distribution of Classes is"))
 print(table(labels$bp_group))
-
 print(sprintf("The Filtered Data Frame is of Size %0.0f by %0.0f",
               nrow(ft_norm), ncol(ft_norm)))
 
