c73ee96830aef8efe51bd3ce416023591ea7518e
zhaohanxiong <zhaohanx@hotmail.com>
2022-12-26 23:22:59 +0000

    updated shell run script

--- src/run_ccrfai_UKB.sh
+++ src/run_ccrfai_UKB.sh
@@ -12,13 +12,23 @@ Rscript preprocess_feature_selection.R
 cd ./NeuroPM
 /home/zhaohanx/Matlab2020a/bin/matlab -nodisplay -nosplash -nodesktop -r "run('run_NeuroPM.m');exit;"
 
-# run post-processing file organization/evaluation
+# run evaluation
 cd ..
-Rscript postprocess_files.R
 Rscript postprocess_eval_model.R
 
 # run python trajectory visualization/computation
 python postprocess_traj_compute.py --max_traj_num=5 --overlap_threshold=0.8 --color_by="traj"
+
+# perform cross validation
+#bash ./run_fmrib_x_validate.sh
+
+# arrange/tidy up files for post-analysis
+Rscript postprocess_files.R
+
+# perform series of post-analysis visualizations
+cd ../postanalysis
+Rscript ukb_visualize_key_cTI_results.R
+Rscript ukb_visualize_traj.R
 
 # run test cases (in root directory)
 cd ../..
