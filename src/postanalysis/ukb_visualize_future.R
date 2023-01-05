aa25f1beb3a45bf2543b90f5e30704bba64a5ebb
zhaohanxiong <zhaohanx@hotmail.com>
2023-01-05 20:59:36 +0000

    planning how to display results

--- src/postanalysis/ukb_visualize_future.R
+++ src/postanalysis/ukb_visualize_future.R
@@ -4,6 +4,20 @@ library(R.matlab)
 library(gridExtra)
 library(data.table)
 
+var_i = 1
+
+# define columns of interest (visit 1 and 2) to this dataframe
+analyze = c("X22423", # LV stroke volume
+            "X22421", # LV end diastole volume
+            "X25781", # white matter hyperintensities
+            "X25019", # Hippocampus volume (left)
+            "X25020") # Hippocampus volume (right)
+analyze_names = c("LV SV",
+                  "LV EDV",
+                  "WM Hyperintensity",
+                  "Hippocampus Volume (Left)",
+                  "Hippocampus Volume (Right)")
+
 # ------------------------------------------------------------------------------
 # Load Data
 # ------------------------------------------------------------------------------
@@ -63,13 +77,6 @@ pred = data.frame(patid = ukb2$eid, global_pseudotimes2 = pred)
 # ------------------------------------------------------------------------------
 # Merge Data For Analysis
 # ------------------------------------------------------------------------------
-# add columns of interest (visit 1 and 2) to this dataframe
-analyze = c("X22423", # LV stroke volume
-            "X22421", # LV end diastole volume
-            "X25781", # white matter hyperintensities
-            "X25019", # Hippocampus volume (left)
-            "X25020")  # Hippocampus volume (right)
-
 # append 1st imaging visit values to original hyper score df
 scores_analyze = cbind(scores, ukb1[, sapply(analyze, function(s) grep(s, colnames(ukb1), value = TRUE))])
 
@@ -82,7 +89,7 @@ follow_up = merge(scores_analyze, pred_analyze, by.x = "patid", by.y = "patid")
 # ------------------------------------------------------------------------------
 # Produce Basic Plots for Score Distribution
 # ------------------------------------------------------------------------------
-
+# change blood pressure groups into a ordered factor for plotting
 follow_up$bp_group = ordered(follow_up$bp_group, c(1, 0, 2))
 
 # generate plot for predicted vs ground truth distribution
@@ -109,22 +116,22 @@ quit(save = "no")
 # ------------------------------------------------------------------------------
 # Aggregate Data for Plots
 # ------------------------------------------------------------------------------
-# iterate through all variables to analyze
-f = paste0("X", future_cols[var_i])
-f_name = weights$name[grep(f, weights$Var1)]
-
 # define 2 variable columns to analyze
-var_1st = paste0(f, ".2.0")
-var_2nd = paste0(f, ".3.0")
+var_1st = grep(paste0(analyze[var_i], "\\.2\\."),
+               colnames(follow_up), value = TRUE)
+var_2nd = grep(paste0(analyze[var_i], "\\.3\\."),
+               colnames(follow_up), value = TRUE)
 
 # subset and remove missing values
-df_plot = df[, c("score", var_1st, var_2nd)]
+df_plot = follow_up[, c("global_pseudotimes", "global_pseudotimes2",
+                        var_1st, var_2nd)]
 df_plot = df_plot[!is.na(df_plot[, var_1st]) & !is.na(df_plot[, var_2nd]), ]
 
 # convert from wide to long format for the 2 variables
-df_plot = data.frame(score = as.factor(rep(df_plot$score, 2)),
+df_plot = data.frame(score = c(df_plot$global_pseudotimes, df_plot$global_pseudotimes),
                      var   = c(df_plot[, var_1st], df_plot[, var_2nd]),
-                     visit = as.factor(rep(c("first", "repeat"), each = nrow(df_plot))))
+                     visit = as.factor(rep(c("1st", "2nd"), each = nrow(df_plot))))
+df_plot$score = cut(df_plot$score, breaks = seq(0, 1, length = 11))
 
 # aggregate by interval means, convert intervals back to numeric (mid-point)
 df_plot2 = aggregate(list(y = df_plot$var),
@@ -138,8 +145,8 @@ df_plot2 = aggregate(list(y = df_plot$var),
 p1 = ggplot(df_plot, aes(x = score, y = var, fill = visit)) + 
         geom_boxplot() +
         ggtitle(sprintf("%s vs Hyper Score (1st & 2nd Visit)",
-                        toTitleCase(f_name))) +
-        ylab(toTitleCase(f_name)) + 
+                        toTitleCase(analyze_names[var_i]))) +
+        ylab(toTitleCase(analyze_names[var_i])) + 
         xlab("Hyper Score") +
         scale_fill_brewer(palette = "Dark2") +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
@@ -150,15 +157,15 @@ p2 = ggplot(df_plot2, aes(x = x, y = y, group = visit, color = visit)) +
         geom_smooth(orientation = "x", span = 15,
                     linewidth = 2, se = FALSE, fullrange = TRUE) +
         ggtitle(sprintf("%s vs Hyper Score (1st & 2nd Visit)",
-                        toTitleCase(f_name))) +
-        ylab(sprintf("%s (averaged per interval)", toTitleCase(f_name))) + 
+                        toTitleCase(analyze_names[var_i]))) +
+        ylab(sprintf("%s (averaged per interval)", toTitleCase(analyze_names[var_i]))) + 
         xlab("Hyper Score [0-1]") +
         scale_color_brewer(palette = "Dark2") +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
               plot.title = element_text(size = 15, face = "bold"))
 
 # start offline plot
-png("plots/temp_future.png", width = 1200, height = 600)
+png("plots/temp_follow_up.png", width = 1200, height = 600)
 
 # mutli-plot
 grid.arrange(p1, p2, ncol = 2)
